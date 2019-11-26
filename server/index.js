16cefe373e9ffbc5ef2176064050d3ea6ca87fa8
Feelsofly <39503879+Feelsofly@users.noreply.github.com>
2019-11-25 22:00:20 -0500

    Started Vue Frontend

--- server/index.js
+++ server/index.js
@@ -12,6 +12,6 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts)
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server started on port ${port}`))
