80948651e21e34e545fe038dd70e845df1c623b8
Feelsofly <39503879+Feelsofly@users.noreply.github.com>
2019-11-25 21:00:49 -0500

    Initial backend created

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
 
 ///Middleware
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 app.use(cors())
 
 const posts = require('./routes/api/posts');
